8558be1a7b1ef056fa77e81722bd012e5148e74d
Nusrat <nusretibragimli@gmail.com>
2021-04-04 23:42:10 +0400

    first

--- app.js
+++ app.js
@@ -4,140 +4,146 @@
     let nail = document.querySelectorAll('[href="#nail"]')
     let hobbi = document.querySelectorAll('[href="#hobbi"]')
     let contact = document.querySelectorAll('[href="#contact"]')
-    console.log(anaSehife);
+console.log(anaSehife);
+    
+
+function sc() {
+  if (window.pageYOffset >= 0 && window.pageYOffset<787 ) {
+    anaSehife[0].classList.add("text-active")
+    anaSehife[1].classList.add("text-active")
+    nail[0].classList.remove("text-active")
+    nail[1].classList.remove("text-active")
+    hobbi[0].classList.remove("text-active")
+    hobbi[1].classList.remove("text-active")
+    contact[0].classList.remove("text-active")
+    contact[1].classList.remove("text-active")
+ 
+
+}
+else if(window.pageYOffset>=787 && window.pageYOffset<=2750){
+    nail[0].classList.add("text-active")
+    nail[1].classList.add("text-active")
+
+    anaSehife[0].classList.remove("text-active")
+    anaSehife[1].classList.remove("text-active")
+    hobbi[0].classList.remove("text-active")
+    hobbi[1].classList.remove("text-active")
+    contact[0].classList.remove("text-active")
+    contact[1].classList.remove("text-active")
+
+}
+
+else if(window.pageYOffset>=2750 && window.pageYOffset<2850){
+    hobbi[0].classList.add("text-active")
+    hobbi[1].classList.add("text-active")
+    anaSehife[0].classList.remove("text-active")
+    anaSehife[1].classList.remove("text-active")
+    nail[0].classList.remove("text-active")
+    nail[1].classList.remove("text-active")
+    contact[0].classList.remove("text-active")
+    contact[1].classList.remove("text-active")
+
+}
+else if(window.pageYOffset>=2850){
+    contact[0].classList.add("text-active")
+    contact[1].classList.add("text-active")
+
+    anaSehife[0].classList.remove("text-active")
+    anaSehife[1].classList.remove("text-active")
+
+    nail[0].classList.remove("text-active")
+    nail[1].classList.remove("text-active")
+
+}
+
+
+
+
+if (window.pageYOffset >= 35) {
+    document.querySelector(".navbar-container").classList.add("navbar-white");
+  } else {
+    document.querySelector(".navbar-container").classList.remove("navbar-white");
+
+  }
+
+
+
+
+if (window.pageYOffset >= 222) {
+document.querySelector(".upup").classList.remove("invisible");
+document.querySelector(".upup").classList.add("visiblee");
+} else {
+document.querySelector(".upup").classList.remove("visiblee");
+document.querySelector(".upup").classList.add("invisible");
+}
+if (window.pageYOffset >= 222 && window.pageYOffset <= 1180) {
+document.querySelectorAll(".d-none").forEach((element) => {
+  element.classList.add("visible");
+  element.classList.remove("d-none");
+});
+} else {
+document.querySelectorAll(".visible").forEach((element) => {
+  element.classList.add("d-none");
+  element.classList.remove("visible");
+});
+}
+
+if (window.pageYOffset >= 722 && window.pageYOffset <= 1680) {
+document.querySelectorAll(".d-none1").forEach((element) => {
+  element.classList.add("visible1");
+  element.classList.remove("d-none1");
+});
+} else {
+document.querySelectorAll(".visible1").forEach((element) => {
+  element.classList.add("d-none1");
+  element.classList.remove("visible1");
+});
+}
+
+if (window.pageYOffset >= 1222 && window.pageYOffset <= 2180) {
+document.querySelectorAll(".d-none2").forEach((element) => {
+  element.classList.add("visible2");
+  element.classList.remove("d-none2");
+});
+} else {
+document.querySelectorAll(".visible2").forEach((element) => {
+  element.classList.add("d-none2");
+  element.classList.remove("visible2");
+});
+}
+
+if (window.pageYOffset >= 1722 && window.pageYOffset <= 2680) {
+document.querySelectorAll(".d-none3").forEach((element) => {
+  element.classList.add("visible3");
+  element.classList.remove("d-none3");
+});
+} else {
+document.querySelectorAll(".visible3").forEach((element) => {
+  element.classList.add("d-none3");
+  element.classList.remove("visible3");
+});
+}
+
+if (window.pageYOffset >= 2222 && window.pageYOffset <= 3180) {
+document.querySelectorAll(".d-none4").forEach((element) => {
+  element.classList.add("visible4");
+  element.classList.remove("d-none4");
+});
+} else {
+document.querySelectorAll(".visible4").forEach((element) => {
+  element.classList.add("d-none4");
+  element.classList.remove("visible4");
+});
+}
+}
 
 window.onscroll = () => {
 
+sc()
 
-    console.log(window.pageYOffset);
-    if (window.pageYOffset >= 0 && window.pageYOffset<787 ) {
-        anaSehife[0].classList.add("text-active")
-        anaSehife[1].classList.add("text-active")
-        nail[0].classList.remove("text-active")
-        nail[1].classList.remove("text-active")
-        hobbi[0].classList.remove("text-active")
-        hobbi[1].classList.remove("text-active")
-        contact[0].classList.remove("text-active")
-        contact[1].classList.remove("text-active")
-     
-
-    }
-    else if(window.pageYOffset>=787 && window.pageYOffset<=2750){
-        nail[0].classList.add("text-active")
-        nail[1].classList.add("text-active")
-
-        anaSehife[0].classList.remove("text-active")
-        anaSehife[1].classList.remove("text-active")
-        hobbi[0].classList.remove("text-active")
-        hobbi[1].classList.remove("text-active")
-        contact[0].classList.remove("text-active")
-        contact[1].classList.remove("text-active")
-
-    }
-
-    else if(window.pageYOffset>=2750 && window.pageYOffset<2850){
-        hobbi[0].classList.add("text-active")
-        hobbi[1].classList.add("text-active")
-        anaSehife[0].classList.remove("text-active")
-        anaSehife[1].classList.remove("text-active")
-        nail[0].classList.remove("text-active")
-        nail[1].classList.remove("text-active")
-        contact[0].classList.remove("text-active")
-        contact[1].classList.remove("text-active")
-
-    }
-    else if(window.pageYOffset>=2850){
-        contact[0].classList.add("text-active")
-        contact[1].classList.add("text-active")
-
-        anaSehife[0].classList.remove("text-active")
-        anaSehife[1].classList.remove("text-active")
-
-        nail[0].classList.remove("text-active")
-        nail[1].classList.remove("text-active")
-
-    }
-    
-    
-    
-    
-    if (window.pageYOffset >= 35) {
-        document.querySelector(".navbar-container").classList.add("navbar-white");
-      } else {
-        document.querySelector(".navbar-container").classList.remove("navbar-white");
-
-      }
-
-
-
-
-  if (window.pageYOffset >= 222) {
-    document.querySelector(".upup").classList.remove("invisible");
-    document.querySelector(".upup").classList.add("visiblee");
-  } else {
-    document.querySelector(".upup").classList.remove("visiblee");
-    document.querySelector(".upup").classList.add("invisible");
-  }
-  if (window.pageYOffset >= 222 && window.pageYOffset <= 1180) {
-    document.querySelectorAll(".d-none").forEach((element) => {
-      element.classList.add("visible");
-      element.classList.remove("d-none");
-    });
-  } else {
-    document.querySelectorAll(".visible").forEach((element) => {
-      element.classList.add("d-none");
-      element.classList.remove("visible");
-    });
-  }
-
-  if (window.pageYOffset >= 722 && window.pageYOffset <= 1680) {
-    document.querySelectorAll(".d-none1").forEach((element) => {
-      element.classList.add("visible1");
-      element.classList.remove("d-none1");
-    });
-  } else {
-    document.querySelectorAll(".visible1").forEach((element) => {
-      element.classList.add("d-none1");
-      element.classList.remove("visible1");
-    });
-  }
-
-  if (window.pageYOffset >= 1222 && window.pageYOffset <= 2180) {
-    document.querySelectorAll(".d-none2").forEach((element) => {
-      element.classList.add("visible2");
-      element.classList.remove("d-none2");
-    });
-  } else {
-    document.querySelectorAll(".visible2").forEach((element) => {
-      element.classList.add("d-none2");
-      element.classList.remove("visible2");
-    });
-  }
-
-  if (window.pageYOffset >= 1722 && window.pageYOffset <= 2680) {
-    document.querySelectorAll(".d-none3").forEach((element) => {
-      element.classList.add("visible3");
-      element.classList.remove("d-none3");
-    });
-  } else {
-    document.querySelectorAll(".visible3").forEach((element) => {
-      element.classList.add("d-none3");
-      element.classList.remove("visible3");
-    });
-  }
-
-  if (window.pageYOffset >= 2222 && window.pageYOffset <= 3180) {
-    document.querySelectorAll(".d-none4").forEach((element) => {
-      element.classList.add("visible4");
-      element.classList.remove("d-none4");
-    });
-  } else {
-    document.querySelectorAll(".visible4").forEach((element) => {
-      element.classList.add("d-none4");
-      element.classList.remove("visible4");
-    });
-  }
 };
+
+sc()
 let down = document.querySelector(".down");
 down.addEventListener("click", () => {
   let hazirki = window.pageYOffset + 500;
